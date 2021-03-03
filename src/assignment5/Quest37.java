08070dc68c0d354abc915a294e9d46193cf4f4af
dhakalsushma <sxdhakaladh@ualr.edu>
2021-03-02 21:23:44 -0600

    added 37

--- src/assignment5/Quest37.java
+++ src/assignment5/Quest37.java
@@ -2,4 +2,28 @@ package assignment5;
 
 //Write a Program to perform for the following task.
 public class Quest37 {
+    public static void main(String[] args) {
+
+        int [][] arr1 = {{1,2,3},{4,5,6},{7,8,9}};
+        int [][] arr2 ={{4,5,6},{2,7,8},{3,1,9}};
+
+        final int ROWS = 3;
+        final int COLS = 3;
+
+        int[][] sum = new int[ROWS][COLS];
+        for(int i=0; i<ROWS; i++) {
+            for (int j = 0; j < COLS; j++) {
+                sum[i][j] = arr1[i][j] + arr2[i][j];
+            }
+            System.out.println();
+        }
+            System.out.println("Sum of the given matrices is: ");
+            for(int i = 0; i < ROWS; i++) {
+                for (int j = 0; j < COLS; j++) {
+                    System.out.print(sum[i][j] + "    ");
+                }
+                System.out.println();
+            }
+        }
+
 }
